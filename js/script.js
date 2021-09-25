ff1a07cb3aa801699dc7742a69e623c82247a916
jasandgun <jasandgun@gmail.com>
2021-09-25 21:45:04 +0700

    fix: add dark mode that follows user preference

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
+// window.onload = function() {
+//     const themesheet = document.querySelector("#theme-sheet");
+//     var a = document.getElementById("theme-home");
+//     a.onclick = function() {
+//         if (themesheet.getAttribute("href") == "css/style.css") {
+//             themesheet.href = "css/style-dark.css";                    
+//         } else {
+//             themesheet.href = "css/style.css";
+//         }
+//     }
+// };
 window.onload = function() {
-    const themesheet = document.querySelector("#theme-sheet");
-    var a = document.getElementById("theme-home");
-    a.onclick = function() {
-        if (themesheet.getAttribute("href") == "css/style.css") {
-            themesheet.href = "css/style-dark.css";                    
-        } else {
-            themesheet.href = "css/style.css";
-        }
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    const themeToggle = document.getElementById("theme-home");
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme == "dark") {
+        document.body.classList.toggle("dark");
+    } else if (currentTheme == "light") {
+        document.body.classList.toggle("light");
     }
-};
+
+    themeToggle.onclick = function() {
+        if (prefersDarkScheme.matches) {
+            document.body.classList.toggle("light");
+            var theme = document.body.classList.contains("light")
+              ? "light"
+              : "dark";
+          } else {
+            document.body.classList.toggle("dark");
+            var theme = document.body.classList.contains("dark")
+              ? "dark"
+              : "light";
+          }
+          localStorage.setItem("theme", theme);
+    }
+}
